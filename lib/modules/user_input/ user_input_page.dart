d253e46fbb23781566dedeb6dc2082784a731282
Md  Ruhul Amin <md.ruhulamin1863@gmail.com>
2025-11-05 20:43:23 +0600

    project setup  and completed folder structure

---
+++ lib/modules/user_input/ user_input_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserInputPage extends StatelessWidget {
+  const UserInputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
